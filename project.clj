670cc8551ab2b388e15fcd4ff5b2e1266a406674
Frozenlock <frozenlock@gmail.com>
2015-05-27 15:38:26 -0400

    initial 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.frozenlock/reagent-table "0.1.0-SNAPSHOT"
+(defproject org.clojars.frozenlock/reagent-table "0.1.0"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
